b713716d776cf96704fbb49b1b5b31de290d227e
MicaildeJunior <micaoliveira.tecnologo@gmail.com>
2024-06-18 21:45:21 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using Data.Models;
+using Microsoft.Data.SqlClient;
+
+const string connecctionstring = "Data Source=LAPTOP-563RGJKO\\sqlexpress;Initial Catalog=balta;Integrated Security=True;TrustServerCertificate=True";
+
+
+var category = new Category();
+
+var insertSql = "INSERT INTO [Category] VALUES(id, title, url, summary, order, description, featured)";
+
+
+using (var connection = new SqlConnection(connecctionstring))
+{
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+    foreach (var item in categories)
+    {
+        Console.WriteLine($"{item.Id} - {item.Title}");
+    }
+};
